bf866c62cf7e01bce31e57cf74db043a5f5e58ad
Julien DELPUECH <jdelpuec@e2r9p14.42.fr>
2019-03-27 12:27:24 +0100

    delete stdio.h

--- includes/wolf.h
+++ includes/wolf.h
@@ -6,7 +6,7 @@
 /*   By: jdelpuec <jdelpuec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 10:55:34 by jdelpuec          #+#    #+#             */
-/*   Updated: 2019/03/25 14:45:14 by jdelpuec         ###   ########.fr       */
+/*   Updated: 2019/03/27 12:26:18 by jdelpuec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 # include "get_next_line.h"
 # include <time.h>
 # include <signal.h>
-# include <stdio.h>
 
 typedef	struct		s_param
 {
